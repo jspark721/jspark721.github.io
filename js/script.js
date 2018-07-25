a1e6c37929171ad732e08084b77e36744d103af9
Julie Park <jspark721@gmail.com>
2018-07-25 15:10:12 -0700

    git init on other computer

---
+++ js/script.js
@@ -0,0 +1,171 @@
+let background = document.querySelector('.landing__box');
+let enter = document.querySelector('.enter');
+let logo = document.querySelector('.landing__logo');
+let landing = document.querySelector('.landing');
+let main = document.querySelector('#main');
+let menu = document.getElementById('menu');
+let nav = document.querySelector('nav');
+let footer = document.querySelector('footer');
+
+background.style.transform = "scaleX(100) scaleY(90)";
+enter.onclick = close;
+background.onclick = open;
+logo.onclick = open;
+main.style.transform = 'translateY(100%)';
+main.style.opacity = '0';
+menu.style.opacity = '0';
+footer.style.opacity = '0';
+
+//animation for clicking logo
+
+function open() {
+  anime({
+    targets: '.landing__logo',
+    color: '#ffffff',
+    translateY: [
+      {value: '-400%', duration: 400,  delay: 200, easing: 'easeOutQuad'},
+      {value: ['200%', '0%'], duration: 300, easing: 'easeOutExpo'}
+    ]
+  });
+
+  anime({
+  	targets: 'div.landing__box',
+    backgroundColor: '#D1A39E',
+  	scaleX: {value: 100, duration: 800, delay: 500, easing: 'easeOutQuad'},
+  	scaleY: {value: 100, duration: 1200, delay: 200, easing: 'easeOutQuad'},
+    opacity: {value: 1, duration: 800, delay: 100, easing: 'easeOutQuad'}
+  });
+
+  anime({
+  	targets: 'div.landing__title, .landing__content',
+    translateX: 0,
+    opacity: 1,
+    delay: 550,
+    duration: 300,
+    easing: 'easeOutExpo'
+  });
+  anime({
+    targets: '.el',
+    translateX: 0,
+    direction: 'normal',
+    duration: 2000,
+    opacity: 1,
+    delay: (el, i, l) => i * 300
+  });
+  anime({
+    targets: '#main',
+    translateX: '110%',
+    duration: 1500,
+    delay: 400,
+    opacity: {
+      value: 0,
+      easing: 'linear',
+      duration: 800
+    }
+  });
+  menu.style.opacity = '0';
+  footer.style.opacity = '0';
+  landing.style.zIndex = '10';
+};
+
+//animation for clicking enter
+
+function close() {
+  window.scrollTo(0, 0);
+
+  anime({
+    targets: '.landing__logo',
+    easing: 'easeOutQuad',
+    delay: 670,
+    color: '#D1A39E',
+    translateY: [
+      {value: '-400%', duration: 500,  delay: 500, easing: 'easeOutQuad'},
+      {value: ['200%', '0%'], duration: 700, easing: 'easeOutExpo'}],
+      rotate: [{value: 0, duration: 300}, {value: [150,0], duration: 1300, easing: 'easeOutElastic'}
+    ]
+  });
+  anime({
+    targets: 'div.landing__box',
+    backgroundColor: '#fff',
+    delay: 1200,
+    scaleX: {value: 1, duration: 800, delay: 500, easing: 'easeOutExpo'},
+    scaleY: {value: 1, duration: 800, delay: 600, easing: 'easeOutQuad'},
+    opacity: {value: 1, duration: 800, delay: 1000, easing: 'easeOutQuad'}
+  });
+
+  anime({
+    targets: '.el',
+    translateX: '-20%',
+    easing: 'easeOutExpo',
+    delay: 550,
+    duration: 200,
+    opacity: { value: 0, easing: 'linear', duration: 200, delay: 500 }
+  });
+
+  anime({
+  	targets: 'div.landing__title, .landing__content',
+    translateX: '-100%',
+    opacity: 0,
+    delay: 550,
+    duration: 300,
+    easing: 'easeOutExpo'
+  });
+
+  anime({
+    targets: '#main',
+    translateY: 0,
+    duration: 2000,
+    delay: 200,
+    opacity: {
+      value: 1,
+      easing: 'linear',
+      delay: 1200,
+      duration: 800
+    }
+  });
+  anime({
+    targets: '#menu',
+    opacity: {
+      value: 1,
+      easing: 'linear',
+      delay: 200,
+      duration: 800
+    }
+  });
+  anime({
+    targets: 'footer',
+    opacity: {
+      value: 1,
+      easing: 'linear',
+      delay: 1800,
+      duration: 800
+    }
+  });
+  landing.style.zIndex = '0';
+};
+
+/*hamburger menu*/
+function toggleMenu() {
+  menu.classList.toggle('open');
+  nav.classList.toggle('show');
+}
+
+menu.onclick = toggleMenu;
+
+/*smooth scrolling*/
+function scrollToTop() {
+  window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+}
+
+function scrollToBottom() {
+  let height = document.documentElement.scrollHeight;
+  window.scroll({ top: height, behavior: 'smooth' });
+}
+
+function scrollToAbout() {
+  let aboutDiv = document.getElementById('about');
+  let aboutHeight = aboutDiv.scrollHeight + (window.innerHeight + (window.innerHeight/2.2));
+
+  window.scroll({ top: aboutHeight, behavior: 'smooth' });
+  console.log(aboutHeight);
+}
